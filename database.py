bf7a02dec7e51743377aec0e2b0a74a80970260b
Inas Tsabitah <122654070+Tsabitah07@users.noreply.github.com>
2025-12-05 14:04:20 +0700

    fix [IT] vercel

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 DATABASE_URL = "mysql+pymysql://root:@localhost:3306/electronic_calculator"
 
 engine = create_engine(DATABASE_URL, echo=True)
-
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
