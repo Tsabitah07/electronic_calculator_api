1b2843a7818099b31901b46b180aeb42f82e4bef
Inas Tsabitah <122654070+Tsabitah07@users.noreply.github.com>
2025-12-05 14:00:13 +0700

    fix [IT] vercel

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "default_local_url")
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/electronic_calculator"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
