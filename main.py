581cd650b4596e57abcaa2482ab32f427a995313
Inas Tsabitah <122654070+Tsabitah07@users.noreply.github.com>
2025-12-08 16:37:04 +0700

    fix [IT] CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routes import electricity, electronicName, electronic, calculator
 
@@ -12,6 +13,23 @@ openapi_tags = [
 ]
 
 app = FastAPI(openapi_tags=openapi_tags)
+
+# 🔥 Tambahkan ini
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(electricity.router, prefix="/electricity", tags=["Electricity"])
 app.include_router(electronicName.router, prefix="/electronic-name", tags=["Electronic Name"])
 app.include_router(electronic.router, prefix="/electronic-data", tags=["Electronic Data"])
